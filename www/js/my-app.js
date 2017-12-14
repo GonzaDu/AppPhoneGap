92eb2fbed11089c23712c4578a7d46cda9ab7451
Xware <duartegonzalo7@gmail.com>
2017-12-14 17:37:13 -0300

    Listo

---
+++ www/js/my-app.js
@@ -0,0 +1,75 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
+
+$$("#login").click(function(){
+    var usuario=$("#usuario").val();
+    var contrasena=$("#contrasena").val();
+    var dataString="usuario="+usuario+"&contrasena="+contrasena+"&password="+password+"&signup=";
+    if($.trim(fullname).length>0 & $.trim(email).length>0 & $.trim(password).length>0)
+    {
+    $.ajax({
+    type: "POST",
+    url: url,
+    data: dataString,
+    crossDomain: true,
+    cache: false,
+    beforeSend: function(){ $("#signup").val('Connecting...');},
+    success: function(data){
+    if(data=="success")
+    {
+    alert("Thank you for Registering with us! you can login now");
+    }
+    else if(data="exist")
+    {
+    alert("Hey! You alreay has account! you can login with us");
+    }
+    else if(data="failed")
+    {
+    alert("Something Went wrong");
+    }
+    }
+    });
+    }return false;
+    });
+
